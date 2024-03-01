681661ddea9ea44aeade90dc18018e1abdc340e5
cham0mi1e <rose990033@gmail.com>
2024-02-29 19:14:55 -0800

    2024.02.29 19:14

--- src/LongLineCheck.java
+++ src/LongLineCheck.java
@@ -1,5 +1,11 @@
+import java.util.*;
+
 public class LongLineCheck implements Check {
+    private final String errorMessage = "Line contains 100 or more characters.";
+    private final int errorCode = 1001;
+
     public List<Error> lint(Lintee lintee) {
-        Scanner fileScan = new Scanner ()
+        Scanner fileScan = lintee.toScanner();
+
     }
 }
