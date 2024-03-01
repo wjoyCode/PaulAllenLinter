ca851299fdc2fa1a3a11167684dd6405f9ed33c4
wjoyCode <161788477+wjoyCode@users.noreply.github.com>
2024-02-29 19:28:03 -0800

    2024.02.29 19:27

---
+++ src/BreakCheck.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class BreakCheck implements Check {
+    private final String errorMessage = "Line contains forbidden break statement"
+    private final int errorCode = 1001;
+
+    public List<Error> lint(Lintee lintee) {
+        List<String> linteeList = lintee.toList();
+        List<Error> errorList = new ArrayList<>(); 
+        for(int i = 0; i < linteeList.size(); i++) {
+            Scanner lineScan = new Scanner(linteeList.get(i));
+            while(lineScan.hasNext()) {
+                if(lineScan.next().equals("break;")) {
+                    errorList.add(new Error(errorCode, i + 1, errorMessage));
+                }
+            }
+        }
+        return errorList;
+    }
+}
