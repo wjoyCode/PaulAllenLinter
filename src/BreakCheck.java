a576aa9917d457ea036b512f1434d7763870cfcc
wjoyCode <161788477+wjoyCode@users.noreply.github.com>
2024-02-29 19:30:45 -0800

    2024.02.29 19:30

--- src/BreakCheck.java
+++ src/BreakCheck.java
@@ -4,7 +4,7 @@ public class BreakCheck implements Check {
     private final String errorMessage = "Line contains forbidden break statement"
     private final int errorCode = 1001;
 
-    public List<Error> lint(Lintee lintee) {
+    public List<Error> lint(Lintee lintee) throws FileNotFoundException {
         List<String> linteeList = lintee.toList();
         List<Error> errorList = new ArrayList<>(); 
         for(int i = 0; i < linteeList.size(); i++) {
--- src/LongLineCheck.java
+++ src/LongLineCheck.java
@@ -4,7 +4,7 @@ public class LongLineCheck implements Check {
     private final String errorMessage = "Line contains 100 or more characters.";
     private final int errorCode = 1001;
 
-    public List<Error> lint(Lintee lintee) {
+    public List<Error> lint(Lintee lintee) throws fileNotFoundException {
         Scanner fileScan = lintee.toScanner();
         List<Error> errorList = new ArrayList<Error>();
         int lineNum = 1;
